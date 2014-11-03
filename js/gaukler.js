7ec9906f9aafcddaaec7d268cf5b399863d67b35
David Deutsch <skore@valanx.org>
2014-11-03 14:50:56 +0100

    starting conversion to angular, covering basic functionality

--- js/gaukler.js
+++ js/gaukler.js
@@ -1,101 +1,172 @@
-jQuery(function($){
-	var data,
-		loc;
+(function () {
 
-	var initSupersized = function() {
-		$.supersized({
-			// Functionality
-			slide_interval : 4000,
-			transition : 1,
-			transition_speed : 1000,
-			fit_always: 1,
-			horizontal_center: 1,
-			slide_links : "blank",
-			slides : data[loc]
-		});
-	};
+	angular.module('gauklerApp', [
+		'ngAnimate', 'ngTouch', 'ui.router'
+	]);
 
-	var loadgallery = function(event) {
-		if ( typeof this.hash === 'undefined' ) {
-			loc = window.location.hash.slice(1);
-		} else {
-			loc = this.hash.slice(1);
-		}
+	/**
+	 * @name AppCfg
+	 *
+	 * @desc Set up the Application
+	 */
+	function AppCfg( $urlRouterProvider, $stateProvider, $locationProvider )
+	{
+		$urlRouterProvider
+			.otherwise('/');
 
-		if ( $.supersized.vars.slideshow_interval !== false ) {
-			if ($.supersized.vars.in_animation) return false;
+		$stateProvider
+			.state('home', {
+				url: '/',
+				views: {
+					"main": {
+						templateUrl: 'partials/intro.html'
+					}
+				}
+			})
 
-			api.playToggle();
+			.state('gallerie', {
+				url: '/gallerie/{path:.*}',
+				views: {
+					"main": {
+						templateUrl: 'partials/gallery.html'
+					}
+				}
+			})
 
-			$("#supersized li").remove();
+			.state('info', {
+				url: '/info/{path:.*}',
+				views: {
+					"main": {
+						templateUrl: 'partials/doc.html'
+					}
+				}
+			})
 
-			clearInterval($.supersized.vars.slideshow_interval);
+		;
+	}
 
-			initSupersized();
+	AppCfg.$inject = ['$urlRouterProvider', '$stateProvider', '$locationProvider'];
+	angular.module('gauklerApp').config(AppCfg);
 
-			api.playToggle();
-		} else {
-			$("#prevslide, #nextslide, #slidecaption").show();
+	/**
+	 * @name AppRun
+	 *
+	 * @desc Data to prepare when we run the application
+	 */
+	function AppRun( $rootScope, $http )
+	{
+		$rootScope.data = {};
 
-			$("#intro").remove();
-			$("#content").remove();
+		$http.get('data.json')
+			.then(function(data){
+				$rootScope.data = data.data;
+			});
 
-			initSupersized();
-		}
+		$rootScope.docs = [
+			{ key: 'programme', title: 'Programme' },
+			{ key: 'charity', title: 'Charity' },
+			{ key: 'technik', title: 'Technische Fragen' },
+			{ key: 'referenzen', title: 'Referenzen' },
+			{ key: 'kosten', title: 'Kosten' },
+			{ key: 'kontakt', title: 'Kontakt & Buchung' }
+		];
 
-		return true;
-	};
+		$rootScope.galleries = [
+			{ key: 'akrobatik', image: 'start_01', title: 'Akrobatik' },
+			{ key: 'closeup_zaubern', image: 'start_09', title: 'Closeup Zaubern' },
+			{ key: 'clownerie_slapstick', image: 'start_07', title: 'Clownerie & Slapstick' },
+			{ key: 'einrad_stelzen', image: 'start_02', title: 'Einrad & Stelzen' },
+			{ key: 'feuerkunst', image: 'start_03', title: 'Feuerkunst' },
+			{ key: 'hulahoop_swinging', image: 'start_04', title: 'Hulahoop & Swinging' },
+			{ key: 'jonglage', image: 'start_05', title: 'Jonglage' },
+			{ key: 'lebende_skulpturen', image: 'start_06', title: 'Lebende Skulpturen' },
+			{ key: 'walkacts', image: 'start_08', title: 'Walkacts' },
+			{ key: 'zauberei', image: 'start_10', title: 'Zauberei' }
+		];
+	}
 
-	var loadcontent = function(event){
-		if ( typeof this.hash === 'undefined' ) {
-			loc = window.location.hash.slice(1);
-		} else {
-			loc = this.hash.slice(1);
-		}
+	AppRun.$inject = ['$rootScope', '$http'];
+	angular.module('gauklerApp').run(AppRun);
 
-		if ( $.supersized.vars.slideshow_interval ) {
-			if($.supersized.vars.in_animation) return false;
+	/**
+	 * @name NavCtrl
+	 */
+	function NavCtrl( $rootScope, $scope )
+	{
+	}
 
-			if (!$.supersized.vars.is_paused) api.playToggle();
+	NavCtrl.$inject = ['$rootScope', '$scope'];
+	angular.module('gauklerApp').controller('NavCtrl', NavCtrl);
 
-			$("#supersized li").hide();
-		}
 
-		$("#prevslide, #nextslide, #slidecaption").hide();
-		$("#intro").remove();
-		$("#slide-list li").remove();
+	/**
+	 * @name GalleryCtrl
+	 */
+	function GalleryCtrl( $rootScope, $scope, $stateParams )
+	{
+		$scope.list = $rootScope.data[$stateParams.path];
 
-		$("#content").remove();
-		$("#content-wrap").append('<div id="content"></div>');
-		$("#content").load("data/"+loc+".html", function(){ $("#content a").click(loadgallery); } );
+		var initSupersized = function() {
+			$.supersized({
+				// Functionality
+				slide_interval : 4000,
+				transition : 1,
+				transition_speed : 1000,
+				fit_always: 1,
+				horizontal_center: 1,
+				slide_links : "blank",
+				slides : $scope.list
+			});
+		};
 
-		return true;
-	};
+		var loadgallery = function(event) {
+			if ( $.supersized.vars.slideshow_interval !== false ) {
+				if ($.supersized.vars.in_animation) return false;
 
-	var redirect = function(event){
-// 					window.navigate(event.currentTarget.hash);
-		window.location.hash = "#" + event.currentTarget.hash.slice(1);
-		window.location.reload(true);
-	};
+				api.playToggle();
 
-	$.getJSON("data.json", function(json) {
-		data = json;
+				clearInterval($.supersized.vars.slideshow_interval);
 
-		$("#prevslide, #nextslide").hide();
+				initSupersized();
 
-		$("#menu-main a, .picture_collection a").click(redirect);
-		$("#menu-sub a, .button_collection a").click(redirect);
-
-		var sloc = window.location.hash.slice(1);
-
-		if ( sloc ) {
-			if ( $.inArray(sloc, ["programme","charity","technik","referenzen","kosten","kontakt"] ) > -1 ) {
-				loadcontent();
+				api.playToggle();
 			} else {
-				loadgallery();
+				initSupersized();
 			}
-		} else {
-			$("#intro").show();
-		}
-	});
-});
+
+			return true;
+		};
+
+		var loadcontent = function(event){
+			if ( $.supersized.vars.slideshow_interval ) {
+				if($.supersized.vars.in_animation) return false;
+
+				if (!$.supersized.vars.is_paused) api.playToggle();
+			}
+
+			return true;
+		};
+
+		loadgallery();
+	}
+
+	GalleryCtrl.$inject = ['$rootScope', '$scope', '$stateParams'];
+	angular.module('gauklerApp').controller('GalleryCtrl', GalleryCtrl);
+
+
+	/**
+	 * @name DocCtrl
+	 */
+	function DocCtrl( $scope, $stateParams, $http, $sce )
+	{
+		$http.get('data/' + $stateParams.path + ".html")
+			.then(function(data){
+				$scope.content = $sce.trustAsHtml(data.data);
+			});
+	}
+
+	DocCtrl.$inject = ['$scope', '$stateParams', '$http', '$sce'];
+	angular.module('gauklerApp').controller('DocCtrl', DocCtrl);
+
+
+})();
